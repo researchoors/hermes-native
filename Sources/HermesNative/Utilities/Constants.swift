29dc7ac41e96f6efa2a645fbbf7c50ca7cf6fbeb
Hank Bob <hankbob@researchoors.com>
2026-04-29 10:14:19 +0700

    feat: add production gateway URL constant

--- Sources/HermesNative/Utilities/Constants.swift
+++ Sources/HermesNative/Utilities/Constants.swift
@@ -2,6 +2,7 @@ import Foundation
 
 enum Constants {
     static let defaultGatewayURL = "ws://127.0.0.1:8642/v1/ws"
+    static let productionGatewayURL = "wss://gateway.model-optimizors.com/v1/ws"
     static let defaultGatewayPort = 8642
     static let wsPath = "/v1/ws"
     static let maxMessageLength = 1_000_000
